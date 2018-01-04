f5e9d655fb2711c2170670a114f4e818bc03ecd3
Amit Stein <amit.stein@southworks.com>
2018-01-04 17:51:48 -0300

    Created Test project for BotFramework.Connector project

---
+++ src/SDKs/BotFramework/dataPlane/Connector/BotFramework.Connector.Tests/BotConnectorTestBase.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Azure.BotFramework.Connector.Models;
+using Microsoft.Azure.Test.HttpRecorder;
+using Microsoft.Rest.ClientRuntime.Azure.TestFramework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Microsoft.Azure.BotFramework.Connector.Tests
+{
+    public class BotConnectorTestBase
+    {
+
+    }
+}
---
+++ src/SDKs/BotFramework/dataPlane/Connector/BotFramework.Connector.Tests/ConversationsTest.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Azure.BotFramework.Connector.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace Microsoft.Azure.BotFramework.Connector.Tests
+{
+    public class ConversationsTest : BotConnectorTestBase
+    {
+        [Fact]
+        public void FirstTest()
+        {
+            Assert.True(true);
+        }
+    }
+}
