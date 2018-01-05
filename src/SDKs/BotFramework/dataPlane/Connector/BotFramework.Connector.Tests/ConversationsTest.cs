760a3c42183e969d0c234a40122138a32d20abd6
Amit Stein <amit.stein@southworks.com>
2018-01-05 15:53:49 -0300

    Update tests with MockContext playback & recorder

--- src/SDKs/BotFramework/dataPlane/Connector/BotFramework.Connector.Tests/ConversationsTest.cs
+++ src/SDKs/BotFramework/dataPlane/Connector/BotFramework.Connector.Tests/ConversationsTest.cs
@@ -1,18 +1,63 @@
-﻿using Microsoft.Azure.BotFramework.Connector.Models;
-using System;
-using System.Collections.Generic;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit;
-
-namespace Microsoft.Azure.BotFramework.Connector.Tests
+﻿namespace Connector.Tests
 {
-    public class ConversationsTest : BotConnectorTestBase
+    using System.Threading.Tasks;
+    using Microsoft.Azure.BotFramework.Connector;
+    using Microsoft.Azure.BotFramework.Connector.Models;
+    using Xunit;
+
+    public class ConversationsTest : BaseTest
     {
         [Fact]
-        public void FirstTest()
+        public void CreateConversation()
         {
-            Assert.True(true);
+            var activity = new Activity()
+            {
+                Type = ActivityType.Message,
+                Recipient = User,
+                FromProperty = Bot,
+                Text = "TEST Create Conversation"
+            };
+
+            var param = new ConversationParameters()
+            {
+                Members = new ChannelAccount[] { User },
+                Bot = Bot,
+                Activity = activity
+            };
+
+            UseClientFor(async client =>
+            {
+                var result = await client.Conversations.CreateConversationAsync(param);
+                Assert.NotNull(result.ActivityId);
+            });
+        }
+
+        [Fact]
+        public void CreateConversationWithInvalidBot()
+        {
+            var invalidBot = new ChannelAccount() { Id = "invalid-id" }; ;
+
+            var activity = new Activity()
+            {
+                Type = ActivityType.Message,
+                Recipient = User,
+                FromProperty = Bot,
+                Text = "TEST Create Conversation"
+            };
+
+            var param = new ConversationParameters()
+            {
+                Members = new ChannelAccount[] { User },
+                Bot = invalidBot,
+                Activity = activity
+            };
+
+            UseClientFor(async client =>
+            {
+                var ex = await Assert.ThrowsAsync<ErrorResponseException>(() => client.Conversations.CreateConversationAsync(param));
+                Assert.Equal("ServiceError", ex.Body.Error.Code);
+                Assert.StartsWith("Invalid userId", ex.Body.Error.Message);
+            });
         }
     }
 }
